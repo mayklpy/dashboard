38b19706ea4a0df973d66dd4d97980070386e3e5
mchlrtnz <mchlrtnz@gmail.com>
2020-01-10 22:29:25 -0500

    installed destyle.css as dependency for resetting styles and updated app filess

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,5 +1,8 @@
+import "../../../node_modules/destyle.css/destyle.css";
 import "../styles/index.scss";
 
 if (module.hot) {
   module.hot.accept();
 }
+
+console.log("index.js loaded...");
--- app/scripts/index.js
+++
@@ -1,5 +0,0 @@
-import "../styles/index.scss";
-
-if (module.hot) {
-  module.hot.accept();
-}
